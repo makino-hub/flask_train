45bc98f3879874886669fed3b6339f533371133c
makino-hub <makinokoudai.vn@gmail.com>
2023-08-15 11:36:43 +0900

    ルーティングの追記

--- myproject/app.py
+++ myproject/app.py
@@ -5,7 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World koudai !</p>"
+    return "<p>Hello, World !</p>"
+
+@app.route("/guri")
+def hello_guri():
+    return "<p>Hello, guri !</p>"
+
+@app.route("/gura")
+def hello_gura():
+    return "<p>Hello, gura !</p>"
 
 if __name__ == "__main__":
     app.run()
