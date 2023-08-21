2c2cbeb8c0f3ad1a3c5e0558634df57aa2b62e03
makino-hub <makinokoudai.vn@gmail.com>
2023-08-21 19:19:49 +0900

    ログインから新規登録まで全ての機能を実装

--- myproject/app.py
+++ myproject/app.py
@@ -3,12 +3,81 @@ from flask import Flask
 from flask import render_template,g,redirect,request
 import sqlite3
 DATABASE = "flaskmemo.db"
+from flask_login import UserMixin,LoginManager,login_required,login_user,logout_user
+import os
+from werkzeug.security import generate_password_hash,check_password_hash
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
+class User(UserMixin):
+    def __init__(self,userid):
+        self.id = userid
+##ログイン       
+@login_manager.user_loader   
+def load_user(userid):
+    return User(userid)
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect('/login')
+
+@app.route('/logout',methods=['GET'])
+def logout():
+    logout_user()
+    return redirect('/login')
+
+@app.route('/signup',methods=['POST','GET'])
+def signup():
+    error_message=''
+    if request.method == 'POST':
+        userid = request.form.get('userid')
+        password = request.form.get('password')
+        pass_hash = generate_password_hash(password,method='sha256')
+        
+        db = get_db()
+        user_check = get_db().execute("select userid from user where userid=?",[userid,]).fetchall()
+        if not user_check:
+            db.execute(
+                "insert into user (userid,password) values(?,?)",
+                [userid,pass_hash]
+            )
+            db.commit()
+            return redirect('/login')
+        else :
+            error_message = "入力されたIDはすでに利用されています。"
+    return render_template('signup.html',error_message=error_message)
+
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    error_message = ""
+    userid=''
+    
+    if request.method == "POST":
+        userid = request.form.get('userid')
+        password = request.form.get('password')
+        # ログインのチェック
+        user_data = get_db().execute("select password from user where userid=?",[userid,]).fetchone()
+        if user_data is not None:
+            if check_password_hash(user_data[0],password):
+                user = User(userid)
+                login_user(user)
+                return redirect('/')
+        error_message = '入力されたIDもしくはパスワードが誤っています'
+            
+        
+    return render_template('login.html',userid=userid,error_message=error_message)
+    
+
+
+###
 
 
 @app.route("/")
+@login_required ##ログインしているかチェックする
 def top():
     memo_list = get_db().execute("select id,title,body from memo").fetchall()
     
@@ -68,10 +137,19 @@ def get_db():
     if not hasattr(g,'sqlite_db') :
         g.sqlite_db = connect_db()
     return g.sqlite_db
+
+
+
+
+
+
     
     
 # 仮装環境に入るコマンド
 # . .venv/bin/activate
+
+# 仮装環境を抜ける
+# deactivate
 
 # flaskを実行するコマンド
 # export FLASK_APP=app
